4b38f77e40b4453a2c55d47fe3db711af639f486
Emile Bex <emile.bex@gmail.com>
2022-04-08 19:17:39 +0200

    [EN-3882] feat(sms): implement phone validation

---
+++ src/components/forms/fields/PhoneInput.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FormValidatorErrorMessage from 'src/components/forms/FormValidatorErrorMessage';
+import PhoneInputWithCountry from 'react-phone-number-input';
+
+const PhoneInput = ({
+  id,
+  name,
+  placeholder,
+  title,
+  type,
+  valid,
+  value,
+  onChange,
+  disabled,
+  hidden,
+  autocomplete,
+}) => {
+  const update = (eventValue) => {
+    onChange(eventValue);
+  };
+
+  return (
+    <div
+      className={`uk-form-controls ${
+        valid !== undefined ? 'uk-padding-remove-bottom' : ''
+      } uk-padding-small uk-padding-remove-left uk-padding-remove-right ${
+        hidden ? ' uk-hidden' : ''
+      }`}
+    >
+      <label className="uk-form-label stay-small" htmlFor={id}>
+        {title}
+      </label>
+      <PhoneInputWithCountry
+        defaultCountry="FR"
+        title={title}
+        name={name}
+        type={type}
+        id={id}
+        value={value || ''}
+        placeholder={placeholder || 'Tapez votre numéro'}
+        onChange={(event) => {
+          return update(event);
+        }}
+        className={`uk-input uk-form-large ${
+          valid !== undefined && valid.isInvalid ? 'uk-form-danger' : ''
+        }`}
+        disabled={disabled}
+        autoComplete={autocomplete}
+      />
+      <FormValidatorErrorMessage validObj={valid} />
+    </div>
+  );
+};
+
+PhoneInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  valid: PropTypes.shape({
+    isInvalid: PropTypes.bool,
+    message: PropTypes.string,
+  }),
+  value: PropTypes.string,
+  disabled: PropTypes.bool,
+  hidden: PropTypes.bool,
+  autocomplete: PropTypes.string,
+};
+
+PhoneInput.defaultProps = {
+  placeholder: 'Tapez votre texte',
+  valid: undefined,
+  value: '',
+  disabled: false,
+  hidden: false,
+  autocomplete: 'tel',
+};
+
+export default PhoneInput;
