df4f929ce474b2fd3ef2e6909f19c3a8ed2ac750
Emile Bex <emile.bex@gmail.com>
2022-04-14 16:07:49 +0200

    fix(fb pixel): put back candidate event on click form link

--- src/components/partials/StepsToJoin.js
+++ src/components/partials/StepsToJoin.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Section } from 'src/components/utils';
 import Button from 'src/components/utils/Button';
 import { gaEvent } from 'src/lib/gtag';
-import { GA_TAGS } from 'src/constants/tags';
+import { FB_TAGS, GA_TAGS } from 'src/constants/tags';
 import { SharePartial } from 'src/components/partials/index';
 import { IconNoSSR } from 'src/components/utils/Icon';
+import { fbEvent } from 'src/lib/fb';
 
 const StepsToJoin = () => {
   return (
@@ -24,7 +25,7 @@ const StepsToJoin = () => {
           newTab
           onClick={() => {
             gaEvent(GA_TAGS.PAGE_TRAVAILLER_DEPOSER_CANDIDATURE_CLIC);
-            // fbEvent(FB_TAGS.CANDIDATE_REGISTRATION);
+            fbEvent(FB_TAGS.CANDIDATE_REGISTRATION);
           }}
           href={process.env.AIRTABLE_LINK_PROFESSIONAL_REINTEGRATION}
         >
