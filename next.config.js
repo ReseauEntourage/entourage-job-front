763f891f704175019bd9c8c731a1cc3e14927ff4
Emile Bex <emile.bex@gmail.com>
2022-04-15 14:59:53 +0200

    [EN-4232] feat(cookies): put automatic library detection

--- next.config.js
+++ next.config.js
@@ -9,6 +9,52 @@ const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 const dev = process.env.NODE_ENV !== 'production';
 
+/* const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'unsafe-inline';
+  child-src 'self';
+  style-src 'self';
+  font-src 'self';
+  img-src 'self' ${process.env.AWSS3_CDN_URL.replace('https://', '')} ${
+  process.env.CDN_URL ? process.env.CDN_URL.replace('https://', '') : ''
+};
+`; */
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: '',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  /* {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  }, */
+];
+
 module.exports = withLess({
   lessOptions: {
     javascriptEnabled: true,
@@ -77,6 +123,14 @@ module.exports = withLess({
         source: '/boite-a-outils',
         destination: process.env.TOOLBOX_URL,
         permanent: false,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
       },
     ];
   },
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
 import { withRouter } from 'next/router';
@@ -7,7 +8,6 @@ import Header from 'src/components/headers/Header';
 import HeaderConnected from 'src/components/headers/HeaderConnected';
 import { addPrefix } from 'src/utils';
 import Script from 'next/script';
-import CookieManager, { TAC_CDN_URL } from 'src/components/CookieManager';
 
 const Layout = ({
   children,
@@ -21,8 +21,9 @@ const Layout = ({
   noIndex,
   isBackoffice,
 }) => {
-  const [cookieManagerReady, setCookieManagerReady] = useState(false);
   const isPDF = router.asPath.includes('/pdf/');
+
+  const domain = process.env.SERVER_URL.replace(/https:\/\/|http:\/\//g, '');
 
   return (
     <>
@@ -75,16 +76,73 @@ const Layout = ({
       {children}
       {!isPDF && !isBackoffice && <Footer />}
       <Script
-        src={`${TAC_CDN_URL}/tarteaucitron.min.js`}
         strategy="afterInteractive"
-        onLoad={() => {
-          setCookieManagerReady(true);
-        }}
-        onError={(err) => {
-          console.error(err);
-        }}
+        src={`https://tarteaucitron.io/load.js?domain=${domain}&uuid=0e7dccd2edb0f870afc26ab86d989e93ef6da0a9`}
       />
-      {cookieManagerReady && <CookieManager />}
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      {/* LinkedIn Insight Tag */}
+      <Script id="linkedin-analytics" strategy="afterInteractive">
+        {`
+          _linkedin_partner_id = "${process.env.LINKEDIN_PARTNER_ID}";
+          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          (function(l) {
+          if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+          window.lintrk.q=[]}
+          var s = document.getElementsByTagName("script")[0];
+          var b = document.createElement("script");
+          b.type = "text/javascript";b.async = true;
+          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+          s.parentNode.insertBefore(b, s);})(window.lintrk);
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          alt=""
+          src={`https://px.ads.linkedin.com/collect/?pid=${process.env.LINKEDIN_PARTNER_ID}&fmt=gif`}
+        />
+      </noscript>
+      {/* Facebook Pixel */}
+      <Script strategy="afterInteractive" id="facebook-analytics">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${process.env.FB_PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${process.env.FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
     </>
   );
 };
