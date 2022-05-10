e55a73f52a80d4ad6a972e13e1b04c1f380457f3
Emile Bex <emile.bex@gmail.com>
2022-05-10 17:39:34 +0200

    [EN-4752] feat(password): update forms with stricter password validation (#36)
    * [EN-4752] feat(password): update forms with stricter password validation
    
    * [EN-4752] feat(password): add numbers in criteria list

--- src/pages/reset/[id]/[token].js
+++ src/pages/reset/[id]/[token].js
@@ -1,94 +1,104 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
 import Layout from 'src/components/Layout';
 import { Button, Section } from 'src/components/utils';
-import schema from 'src/components/forms/schema/formResetPassword.json';
+import schema from 'src/components/forms/schema/formResetPassword';
 import FormWithValidation from 'src/components/forms/FormWithValidation';
 import Api from 'src/Axios';
 import { IconNoSSR } from 'src/components/utils/Icon';
-import LoadingScreen from 'src/components/backoffice/cv/LoadingScreen';
+import PasswordCriterias from 'src/components/PasswordCriterias';
+import PropTypes from 'prop-types';
 
-const ResetPasswordPage = () => {
-  const {
-    isReady,
-    push,
-    query: { id, token },
-  } = useRouter();
-
-  const [valide, setValide] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (isReady && id && token) {
-      Api.get(`/auth/reset/${id}/${token}`)
-        .then(() => {
-          setValide(true);
-        })
-        .catch(() => {
-          setValide(false);
-          console.log('Lien non valide');
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    }
-  }, [id, isReady, token]);
+const ResetPasswordPage = ({ valid, id, token, isCreation }) => {
+  const { push } = useRouter();
 
   return (
-    <Layout title="Réinitialisation de mot de passe - LinkedOut">
+    <Layout
+      title={`${
+        isCreation ? 'Création' : 'Réinitialisation'
+      } de mot de passe - LinkedOut`}
+    >
       <Section size="large" style="muted">
         <div className="uk-flex uk-flex-center uk-flex-middle">
-          {loading ? (
-            <LoadingScreen />
-          ) : (
-            <>
-              {valide ? (
-                <div className="uk-width-1-2@m uk-card uk-card-default uk-card-body">
-                  <h1>Réinitialisation de mot de passe</h1>
-                  <FormWithValidation
-                    formSchema={schema}
-                    onSubmit={({ newPassword, confirmPassword }, setError) => {
-                      return Api.post(`/auth/reset/${id}/${token}`, {
-                        newPassword,
-                        confirmPassword,
+          <>
+            {valid ? (
+              <div className="uk-width-1-2@m uk-card uk-card-default uk-card-body">
+                <h1>
+                  {isCreation ? 'Création' : 'Réinitialisation'} de mot de passe
+                </h1>
+                <PasswordCriterias />
+                <FormWithValidation
+                  formSchema={schema}
+                  onSubmit={({ newPassword, confirmPassword }, setError) => {
+                    return Api.post(`/auth/reset/${id}/${token}`, {
+                      newPassword,
+                      confirmPassword,
+                    })
+                      .then(() => {
+                        push('/reset/success');
                       })
-                        .then(() => {
-                          push('/reset/success');
-                        })
-                        .catch((err) => {
-                          setError(
-                            err?.response?.data?.error ||
-                              'Une erreur est survenue'
-                          );
-                        });
+                      .catch((err) => {
+                        setError(
+                          err?.response?.data?.error ||
+                            'Une erreur est survenue'
+                        );
+                      });
+                  }}
+                />
+              </div>
+            ) : (
+              <div className="uk-card uk-card-body uk-text-center">
+                <IconNoSSR name="ban" ratio={4} className="uk-text-primary" />
+                <p className="uk-text-lead">
+                  Ce lien ne semble pas valide. Veuillez contacter l&apos;équipe
+                  Linkedout.
+                </p>
+                <div className="uk-flex uk-flex-center">
+                  <Button
+                    style="primary"
+                    onClick={() => {
+                      return push('/');
                     }}
-                  />
+                  >
+                    Retourner à l&apos;accueil
+                  </Button>
                 </div>
-              ) : (
-                <div className="uk-card uk-card-body uk-text-center">
-                  <IconNoSSR name="ban" ratio={4} className="uk-text-primary" />
-                  <p className="uk-text-lead">
-                    Ce lien ne semble pas valide. Veuillez contacter
-                    l&apos;équipe Linkedout.
-                  </p>
-                  <div className="uk-flex uk-flex-center">
-                    <Button
-                      style="primary"
-                      onClick={() => {
-                        return push('/');
-                      }}
-                    >
-                      Retourner à l&apos;accueil
-                    </Button>
-                  </div>
-                </div>
-              )}
-            </>
-          )}
+              </div>
+            )}
+          </>
         </div>
       </Section>
     </Layout>
   );
+};
+
+ResetPasswordPage.getInitialProps = async ({ query }) => {
+  const { id, token, isCreation } = query;
+  const valid = await Api.get(`/auth/reset/${id}/${token}`)
+    .then(() => {
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
+
+  return {
+    valid,
+    id,
+    token,
+    isCreation,
+  };
+};
+
+ResetPasswordPage.propTypes = {
+  valid: PropTypes.bool.isRequired,
+  id: PropTypes.string.isRequired,
+  token: PropTypes.string.isRequired,
+  isCreation: PropTypes.string,
+};
+
+ResetPasswordPage.defaultProps = {
+  isCreation: false,
 };
 
 export default ResetPasswordPage;
