85b7061d64a3813daa7bd38524508678c5b69870
paulcarillion <paul@entourage.social>
2022-10-13 11:31:49 +0200

    facto - util and hook

--- src/components/utils/Inputs/Checkbox/useCheckbox.js
+++ src/components/utils/Inputs/Checkbox/useCheckbox.js
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 export const useCheckbox = (callback, params) => {
   const [checked, setChecked] = useState(false);
-  const handleCheckBox = () => {
+  const handleCheckBox = useCallback(() => {
     if (callback) {
       callback(params);
     }
     setChecked(!checked);
-  };
+  }, [callback, checked, params]);
   return { checked, handleCheckBox };
 };
