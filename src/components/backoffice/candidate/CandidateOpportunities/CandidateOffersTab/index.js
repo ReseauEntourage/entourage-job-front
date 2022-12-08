7adb93c96d7e5a0284cfeb6f25a6d71e91d531ef
PaulEntourage <112417197+PaulEntourage@users.noreply.github.com>
2022-12-08 16:36:14 +0100

    in progress

--- src/components/backoffice/candidate/CandidateOpportunities/CandidateOffersTab/index.js
+++ src/components/backoffice/candidate/CandidateOpportunities/CandidateOffersTab/index.js
@@ -1,28 +1,59 @@
 import React from 'react';
 import Link from 'next/link';
 import { StyledTabsUl } from 'src/components/backoffice/candidate/CandidateOpportunities/CandidateOffersTab/styles';
+import { uuid } from 'uuid/v4';
 
-const CandidateOffersTab = () => {
+const CandidateOffersTab = ({ activeTab }) => {
   const basePath = '/backoffice/candidat/offres/private';
+  const tabs = [
+    {
+      status: [-1],
+      text: 'offres à traiter',
+    },
+    {
+      status: [0],
+      text: 'offres contactées',
+    },
+    {
+      status: [1],
+      text: "offres en phase d'entretien",
+    },
+    {
+      status: [3, 4],
+      text: 'offres abandonnées',
+    },
+    {
+      status: [2],
+      text: 'offres acceptées',
+    },
+  ];
+  console.log(activeTab);
   return (
     <StyledTabsUl>
-      <li>
-        <Link href={`${basePath}?status=-1&bookmarked=true`}>
-          offres à traiter
-        </Link>
-      </li>
-      <li>
-        <Link href={`${basePath}?status=0`}>offres contactées</Link>
-      </li>
-      <li>
-        <Link href={`${basePath}?status=1`}>offres en phase d'entretien</Link>
-      </li>
-      <li>
-        <Link href={`${basePath}?status=3&status=4`}>offres abandonnées</Link>
-      </li>
-      <li>
-        <Link href={`${basePath}?status=2`}>offres acceptées</Link>
-      </li>
+      {tabs.map(({ status, text }, k) => {
+        let queryString = '?';
+        for (let i = 0; i < status.length; i += 1) {
+          queryString += `status=${status[i]}`;
+          if (i < status.length - 1) queryString += '&';
+        }
+        console.log(queryString);
+        return (
+          <li
+            className={
+              JSON.stringify(
+                activeTab.map((t) => {
+                  return t.value;
+                })
+              ) === JSON.stringify(status)
+                ? 'active'
+                : ''
+            }
+            key={`${k}-${uuid}`}
+          >
+            <Link href={`${basePath}${queryString}`}>{text}</Link>
+          </li>
+        );
+      })}
     </StyledTabsUl>
   );
 };
