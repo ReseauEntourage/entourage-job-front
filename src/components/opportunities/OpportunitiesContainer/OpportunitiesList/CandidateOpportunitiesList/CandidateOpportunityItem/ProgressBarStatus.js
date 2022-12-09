cbd0b448d07a914955926159bd7740fe07afebd6
Emile Bex <emile.bex@gmail.com>
2022-12-09 17:12:01 +0100

    [EN-5297] feat(offers): wip action labels

--- src/components/opportunities/OpportunitiesContainer/OpportunitiesList/CandidateOpportunitiesList/CandidateOpportunityItem/ProgressBarStatus.js
+++ src/components/opportunities/OpportunitiesContainer/OpportunitiesList/CandidateOpportunitiesList/CandidateOpportunityItem/ProgressBarStatus.js
@@ -5,16 +5,20 @@ import { Container, Step } from './ProgressBarStatus.styles';
 const ProgressBarStatus = ({ status }) => {
   return (
     <Container>
-      <Step activate />
-      <Step activate={status >= 0} />
-      <Step activate={status >= 1} />
-      <Step activate={status >= 2} />
+      <Step activate={status} />
+      <Step activate={status && status >= 0} />
+      <Step activate={status && status >= 1} />
+      <Step activate={status && status >= 2} />
     </Container>
   );
 };
 
+ProgressBarStatus.defaultProps = {
+  status: null,
+};
+
 ProgressBarStatus.propTypes = {
-  status: PropTypes.number.isRequired,
+  status: PropTypes.number,
 };
 
 export default ProgressBarStatus;
