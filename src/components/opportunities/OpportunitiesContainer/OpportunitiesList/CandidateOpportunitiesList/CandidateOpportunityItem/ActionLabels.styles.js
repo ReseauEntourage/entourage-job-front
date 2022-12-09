3577673ed3e642481e320c03e88194ce099cd9ba
Emile Bex <emile.bex@gmail.com>
2022-12-09 17:12:01 +0100

    [EN-5297] feat(offers): wip action labels

--- src/components/opportunities/OpportunitiesContainer/OpportunitiesList/CandidateOpportunitiesList/CandidateOpportunityItem/ActionLabels.styles.js
+++ src/components/opportunities/OpportunitiesContainer/OpportunitiesList/CandidateOpportunitiesList/CandidateOpportunityItem/ActionLabels.styles.js
@@ -1,40 +1,76 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import { COLORS } from 'src/constants/styles';
 import {
   colorToHoverBackgroundColor,
   colorToHoverColor,
 } from 'src/components/utils/Button/styles';
 
+const slideInRight = keyframes`
+  0% {
+    transform: translateX(1000px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+`;
+
 export const Container = styled.div`
   font-family: Poppins, sans-serif;
   display: flex;
   align-items: center;
-  padding: 6px 15px;
+  padding: 8px 16px;
   background-color: white;
   font-size: 12px;
   line-height: 12px;
   border: 0.5px solid
-    ${(props) => {
-      return COLORS[props.color] || COLORS.primaryOrange;
+    ${({ color }) => {
+      return COLORS[color] || COLORS.primaryOrange;
     }};
-  color: ${(props) => {
-    return COLORS[props.color] || COLORS.primaryOrange;
+  color: ${({ color }) => {
+    return COLORS[color] || COLORS.primaryOrange;
   }} !important;
+
   polygon {
-    fill: ${(props) => {
-      return COLORS[props.color] || COLORS.primaryOrange;
+    fill: ${({ fill, color }) => {
+      return fill ? COLORS[color] || COLORS.primaryOrange : 'transparent';
     }};
   }
+
   border-radius: 25px;
+
   &:hover {
-    cursor: pointer;
-    background-color: ${(props) => {
-      return colorToHoverBackgroundColor[props.color];
-    }};
-    color: ${(props) => {
-      return colorToHoverColor[props.color];
-    }} !important;
+    ${({ disabled }) => {
+      return (
+        !disabled &&
+        css`
+          cursor: pointer;
+          background-color: ${({ color }) => {
+            return colorToHoverBackgroundColor[color];
+          }};
+          color: ${({ color }) => {
+            return colorToHoverColor[color];
+          }} !important;
+        `
+      );
+    }}
+        //animation: ${slideInRight} 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;;;;;;;;;;;
   }
+  /*
+
+  ${({ hoverAnimation }) => {
+    return (
+      hoverAnimation &&
+      css`
+        &:hover {
+          position: fixed;
+          transform: translate(0%, -30%);
+          transition: 0.3s ease-out;
+        }
+      `
+    );
+  }} */
 `;
 
 export const LabelContainer = styled.div`
@@ -42,6 +78,5 @@ export const LabelContainer = styled.div`
 `;
 
 export const IconContainer = styled.div`
-  margin-left: 10px;
-
+  margin-left: 8px;
 `;
