c6021ca6c19a6feed3103364ec80f05f87665fa3
Emile Bex <emile.bex@gmail.com>
2022-12-15 16:11:45 +0100

    [EN-5297] feat(offers): manage scroll on details

--- src/components/backoffice/opportunities/OpportunitiesContainer/OpportunitiesList/CandidateOpportunitiesList/CandidateOpportunitiesList.js
+++ src/components/backoffice/opportunities/OpportunitiesContainer/OpportunitiesList/CandidateOpportunitiesList/CandidateOpportunitiesList.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useQueryParamsOpportunities } from 'src/components/backoffice/opportunities/useQueryParamsOpportunities';
 import { useOpportunityId } from 'src/components/backoffice/opportunities/OpportunitiesContainer/useOpportunityId';
@@ -9,12 +9,46 @@ import { openModal } from 'src/components/modals/Modal';
 import ModalExternalOffer from 'src/components/modals/Modal/ModalGeneric/OfferModals/ModalOffer/ModalExternalOffer';
 import { IconNoSSR } from 'src/components/utils/Icon';
 import { useOpportunityType } from 'src/components/backoffice/opportunities/OpportunitiesContainer/useOpportunityType';
+import { useScrollPosition } from '@n8tb1t/use-scroll-position';
+import { useWindowHeight } from '@react-hook/window-size';
 import { LinkCard, ListContent, ListItem } from '../OpportunitiesList.styles';
+import { usePrevious } from '../../../../../../hooks/utils';
 
-const CandidateOpportunitiesList = ({ opportunities, fetchOpportunities }) => {
+const CandidateOpportunitiesList = ({
+  opportunities,
+  fetchOpportunities,
+  setOffset,
+}) => {
   const queryParamsOpportunities = useQueryParamsOpportunities();
   const opportunityId = useOpportunityId();
   const opportunityType = useOpportunityType();
+
+  const [isAtBottom, setIsAtBottom] = useState(false);
+  const windowHeight = useWindowHeight();
+
+  const prevIsAtBottom = usePrevious(isAtBottom);
+
+  useEffect(() => {
+    if (isAtBottom && isAtBottom !== prevIsAtBottom) {
+      setOffset((prevOffset) => {
+        return prevOffset + 1;
+      });
+    }
+  }, [setOffset, isAtBottom, prevIsAtBottom]);
+
+  useScrollPosition(
+    ({ currPos }) => {
+      if (
+        !isAtBottom &&
+        currPos.y * -1 === document.body.offsetHeight - windowHeight
+      ) {
+        setIsAtBottom(true);
+      } else {
+        setIsAtBottom(false);
+      }
+    },
+    [windowHeight]
+  );
 
   const opportunitiesListContent = opportunities.map((opportunity) => {
     return (
@@ -56,19 +90,25 @@ const CandidateOpportunitiesList = ({ opportunities, fetchOpportunities }) => {
   return (
     <ListContent>
       {opportunitiesListContent}
-      <Button
-        style="primary"
-        color="primaryOrange"
-        dataTestId="candidat-add-offer"
-        onClick={() => {
-          openModal(
-            <ModalExternalOffer fetchOpportunities={fetchOpportunities} />
-          );
-        }}
-      >
-        <IconNoSSR name="plus" ratio="0.8" className="uk-margin-small-right" />
-        Ajouter une offre externe
-      </Button>
+      {opportunityType === 'private' && (
+        <Button
+          style="primary"
+          color="primaryOrange"
+          dataTestId="candidat-add-offer"
+          onClick={() => {
+            openModal(
+              <ModalExternalOffer fetchOpportunities={fetchOpportunities} />
+            );
+          }}
+        >
+          <IconNoSSR
+            name="plus"
+            ratio="0.8"
+            className="uk-margin-small-right"
+          />
+          Ajouter une offre externe
+        </Button>
+      )}
     </ListContent>
   );
 };
@@ -76,6 +116,7 @@ const CandidateOpportunitiesList = ({ opportunities, fetchOpportunities }) => {
 CandidateOpportunitiesList.propTypes = {
   opportunities: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   fetchOpportunities: PropTypes.func.isRequired,
+  setOffset: PropTypes.func.isRequired,
 };
 
 export default CandidateOpportunitiesList;
