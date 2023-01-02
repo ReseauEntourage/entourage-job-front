b33a9dde98ef2970234e041c6151c3c83509a444
PaulEntourage <112417197+PaulEntourage@users.noreply.github.com>
2023-01-02 13:43:17 +0100

    EN-5299: mail contact/relance entreprise FE

--- src/components/utils/Inputs/TextArea/TextArea.js
+++ src/components/utils/Inputs/TextArea/TextArea.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 import { StyledTextAreaContainer } from './TextArea.styles';
 
 const TextArea = ({ label, name, id }) => {
   return (
     <StyledTextAreaContainer>
-      <div className="label">
-        <label>{label}</label>
+      <div className="label" id={id}>
+        <label htmlFor={name}>{label}</label>
       </div>
-      <textarea />
+      <textarea name={name} rows={5} />
     </StyledTextAreaContainer>
   );
+};
+
+TextArea.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default TextArea;
