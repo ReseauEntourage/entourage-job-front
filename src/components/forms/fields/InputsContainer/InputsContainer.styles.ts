b365e11d8cbce45bca9890913ff3244a75f1c1c1
PaulEntourage <112417197+PaulEntourage@users.noreply.github.com>
2023-01-30 17:19:40 +0100

    form add external offer --prettier/lint

--- src/components/forms/fields/InputsContainer/InputsContainer.styles.ts
+++ src/components/forms/fields/InputsContainer/InputsContainer.styles.ts
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
 export const StyledInputsContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: flex-end;
-    justify-content: space-between;
-    .field-container {
-
-    }
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-end;
+  justify-content: space-between;
+  .field-container {
+  }
 `;
