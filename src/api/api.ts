e29111162168a337643aa4e2d949b7dd5be7ad3f
PaulEntourage <112417197+PaulEntourage@users.noreply.github.com>
2023-02-01 17:42:01 +0100

    contact relance entreprise requete api

--- src/api/api.ts
+++ src/api/api.ts
@@ -270,6 +270,10 @@ class APIHandler {
     return this.post('/opportunity/join', params);
   }
 
+  postOpportunityContactEmployer(params: object): Promise<AxiosResponse> {
+    return this.post('/opportunity/contactEmployer', params);
+  }
+
   // put
 
   putOpportunity(
