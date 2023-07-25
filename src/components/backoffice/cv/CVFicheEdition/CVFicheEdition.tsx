d9cde298a78f10abf6018ee3697d11d1eea46fcc
PaulEntourage <paul@entourage.social>
2023-07-25 14:32:18 +0200

    -- in progress

--- src/components/backoffice/cv/CVFicheEdition/CVFicheEdition.tsx
+++ src/components/backoffice/cv/CVFicheEdition/CVFicheEdition.tsx
@@ -65,6 +65,8 @@ export const CVFicheEdition = ({
     }
   }, [prevPreviewGenerating, previewGenerating, updateImage]);
 
+  console.log(cv);
+
   return (
     <Grid childWidths={['1-1']}>
       <Grid childWidths={['1-1']} match>
