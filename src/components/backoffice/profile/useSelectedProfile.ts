8d2e2d7896af0a2c09b601c209cc8ac44e2bff15
PaulEntourage <paul@entourage.social>
2024-01-29 13:19:23 +0100

    fix/2.13.1

--- src/components/backoffice/profile/useSelectedProfile.ts
+++ src/components/backoffice/profile/useSelectedProfile.ts
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import UIkit from 'uikit';
+import { ReduxRequestEvents } from 'src/constants';
 import { useUserId } from 'src/hooks/queryParams/useUserId';
+import { usePrevious } from 'src/hooks/utils';
 import { profilesActions } from 'src/use-cases/profiles';
 import {
   fetchSelectedProfileSelectors,
@@ -9,23 +12,36 @@ import {
 
 export function useSelectedProfile() {
   const userId = useUserId();
+  const prevUserId = usePrevious(userId);
   const dispatch = useDispatch();
 
-  const isFetchedSelectedProfileIdle = useSelector(
-    fetchSelectedProfileSelectors.selectIsFetchSelectedProfileIdle
+  const fetchSelectedProfileStatus = useSelector(
+    fetchSelectedProfileSelectors.selectFetchSelectedProfileStatus
+  );
+  const prevFetchSelectedProfileStatus = usePrevious(
+    fetchSelectedProfileStatus
   );
 
   const selectedProfile = useSelector(selectSelectedProfile);
 
   useEffect(() => {
-    if (isFetchedSelectedProfileIdle && userId) {
+    if (userId && userId !== prevUserId) {
       dispatch(
         profilesActions.fetchSelectedProfileRequested({
           userId: userId as string,
         })
       );
     }
-  }, [dispatch, isFetchedSelectedProfileIdle, userId]);
+  }, [dispatch, userId, prevUserId]);
+
+  useEffect(() => {
+    if (prevFetchSelectedProfileStatus === ReduxRequestEvents.REQUESTED) {
+      if (fetchSelectedProfileStatus === ReduxRequestEvents.FAILED) {
+        UIkit.notification('Une erreur est survenue', 'danger');
+      }
+      dispatch(profilesActions.fetchSelectedProfileReset());
+    }
+  }, [dispatch, fetchSelectedProfileStatus, prevFetchSelectedProfileStatus]);
 
   return {
     selectedProfile,
