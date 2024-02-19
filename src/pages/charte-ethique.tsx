2023185a795ce548ea4a16bfec52c7726faf2273
PaulEntourage <paul@entourage.social>
2024-02-19 16:13:13 +0100

    in progress

---
+++ src/pages/charte-ethique.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CharteEthique = () => {
+  return (
+    <div>CharteEthique</div>
+  )
+}
+
+export default CharteEthique
---
+++ src/pages/conseils-posture.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConseilsPosture = () => {
+  return (
+    <div>ConseilsPosture</div>
+  )
+}
+
+export default ConseilsPosture;
