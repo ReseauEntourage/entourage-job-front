52cd174aee04cf4d5f5930a1ebc8e21e86d05ef2
PaulEntourage <paul@entourage.social>
2024-02-27 10:58:21 +0100

    in progress

--- src/pages/charte-ethique.tsx
+++ src/pages/charte-ethique.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Layout } from 'src/components/Layout'
+import { CharteEthique } from 'src/components/partials/CharteEthique/CharteEthique'
 
-const CharteEthique = () => {
+const CharteEthiquePage = () => {
   return (
-    <div>CharteEthique</div>
+    <Layout title="Charte Ethique - LinkedOut">
+      <CharteEthique />
+    </Layout>
+
   )
 }
 
-export default CharteEthique
+export default CharteEthiquePage;
