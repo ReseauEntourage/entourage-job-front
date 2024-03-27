21ab6d76cccba9a4f089ece7ff91529448e3f036
Emile Bex <emile.bex@gmail.com>
2024-03-27 17:40:12 +0100

    [EN-6820] feat(referent): add contact referent to dashboard

---
+++ src/constants/referents.ts
@@ -0,0 +1,42 @@
+import { ADMIN_ZONES, AdminZone } from 'src/constants/departements';
+
+export interface Referent {
+  name: string;
+  img: string;
+  mail: string;
+}
+
+export const Referents: {
+  [K in AdminZone]: Referent;
+} = {
+  [ADMIN_ZONES.PARIS]: {
+    name: 'Julie P.',
+    img: `julie.jpg`,
+    mail: process.env.ADMIN_CANDIDATES_PARIS as string,
+  },
+  [ADMIN_ZONES.LYON]: {
+    name: 'Céline D.',
+    img: `celine.jpg`,
+    mail: process.env.ADMIN_CANDIDATES_LYON as string,
+  },
+  [ADMIN_ZONES.LILLE]: {
+    name: 'Delphine N.',
+    img: `delphine.jpg`,
+    mail: process.env.ADMIN_CANDIDATES_LILLE as string,
+  },
+  [ADMIN_ZONES.RENNES]: {
+    name: 'Mathilde G.',
+    img: `mathilde.jpg`,
+    mail: process.env.ADMIN_CANDIDATES_RENNES as string,
+  },
+  [ADMIN_ZONES.LORIENT]: {
+    name: 'Mathilde G.',
+    img: `mathilde.jpg`,
+    mail: process.env.ADMIN_CANDIDATES_LORIENT as string,
+  },
+  [ADMIN_ZONES.HZ]: {
+    name: 'Julie P.',
+    img: `julie.jpg`,
+    mail: process.env.ADMIN_CANDIDATES_HZ as string,
+  },
+} as const;
