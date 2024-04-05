b4b1a21ac84e631bb42f114cfce31a75f86babb9
PaulEntourage <paul@entourage.social>
2024-04-05 17:29:14 +0200

    [EN-6971] feat(lko2): onboarding corrections

--- src/components/backoffice/onboarding/Onboarding/forms/schemas/formOnboardingCoachProfile.ts
+++ src/components/backoffice/onboarding/Onboarding/forms/schemas/formOnboardingCoachProfile.ts
@@ -7,12 +7,11 @@ export const formOnboardingCoachProfile: FormSchema<{ description: string }> = {
       id: 'description',
       name: 'description',
       component: 'textarea',
-      title: 'Présentez-vous en quelques mots*',
+      title: 'Présentez-vous en quelques mots',
       showLabel: true,
       placeholder:
         "Retraité actif dans le domaine de la cybersécurité après avoir exercé différentes fonctions en finance, RH, audit et informatique chez Décathlon. Dotée d'une forte capacité d'adaptation et d'un désir d'en apprendre toujours plus, je cherche à donner d'élan à ceux qui ont besoin. ",
       rows: 14,
-      isRequired: true,
       maxLines: { lines: 5, width: 655 },
     },
   ],
--- src/components/forms/schemas/formEditHelps.ts
+++ src/components/forms/schemas/formEditHelps.ts
@@ -14,6 +14,7 @@ export function getFormEditHelps(role: NormalUserRole): FormSchema<{
         component: 'select-list',
         isMulti: true,
         options: ParametresHelpCardContents[role],
+        isRequired: true,
       },
     ],
   };
