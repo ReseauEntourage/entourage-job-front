98f2263a293b24530ed4f1cce2b44d880b0fd6cd
PaulEntourage <112417197+PaulEntourage@users.noreply.github.com>
2024-04-09 15:43:56 +0200

    fix(lko2): storybook (#261)

--- src/components/utils/Cards/ProfileCard/ProfileCard.stories.tsx
+++ src/components/utils/Cards/ProfileCard/ProfileCard.stories.tsx
@@ -1,11 +1,22 @@
 import { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import { Provider } from 'react-redux';
 import { v4 as uuid } from 'uuid';
 import { USER_ROLES } from 'src/constants/users';
-import { ProfileCard } from '.';
+import { store } from 'src/store/store';
+import { ProfileCard, ProfileCardProps } from '.';
+
+const Component = (props: ProfileCardProps) => {
+  return (
+    <Provider store={store}>
+      <ProfileCard {...props} />
+    </Provider>
+  );
+};
 
 const meta = {
   title: 'Profile Card',
-  component: ProfileCard,
+  component: Component,
 } satisfies Meta<typeof ProfileCard>;
 
 export default meta;
