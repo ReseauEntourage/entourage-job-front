048d3c33421e480d227a21f114faed0721c4d2bc
Guillaume Cauchois <guillobits@gmail.com>
2024-07-29 13:09:29 +0200

    [EN-7189] Fix - Get Whatsapp group from api

--- src/components/backoffice/dashboard/DashboardJoinWhatsappCoachModal/DashboardJoinWhatsappCoachModal.tsx
+++ src/components/backoffice/dashboard/DashboardJoinWhatsappCoachModal/DashboardJoinWhatsappCoachModal.tsx
@@ -1,9 +1,8 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 
 import { useModalContext } from 'src/components/modals/Modal';
 import { ModalGeneric } from 'src/components/modals/Modal/ModalGeneric';
 import { Button, Img, SimpleLink } from 'src/components/utils';
-import { WhatsappCoach } from 'src/constants/whatsapps-coach';
 import { useAuthenticatedUser } from 'src/hooks/authentication/useAuthenticatedUser';
 import { StyledImageContainer } from './DashboardJoinWhatsappCoachModal.styles';
 
@@ -11,23 +10,20 @@ export const DashboardJoinWhatsappCoachModal = () => {
   const { onClose } = useModalContext();
   const user = useAuthenticatedUser();
 
-  const whatsappCoach = useMemo(() => {
-    return WhatsappCoach[user.zone];
-  }, [user.zone]);
-
   return (
     <ModalGeneric title="Rejoindre note groupe Whatsapp">
       Pour recevoir des actualités et échanger avec les coachs de votre région,
       scannez le QR code
       <StyledImageContainer>
-        <SimpleLink href={whatsappCoach.url} target="_blank">
+        <SimpleLink href={user.whatsappJoinUrl.url} target="_blank">
           <Img
-            src={whatsappCoach.qrcode_url}
+            src={user.whatsappJoinUrl.qrcode_url}
             alt="QR code Whatsapp"
             width={250}
             height={250}
           />
         </SimpleLink>
+        <p>{user.whatsappJoinUrl.name}</p>
       </StyledImageContainer>
       <div className="uk-modal-footer uk-padding-remove-horizontal uk-padding-remove-bottom uk-margin-medium-top">
         <Button onClick={onClose} style="default">
