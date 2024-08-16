bc774a7bbd21a103160b113e125c79195f3b9ac5
Guillaume Cauchois <guillobits@gmail.com>
2024-08-16 11:56:35 +0200

    [EN-6995] Add - ProfileReportUserModal

---
+++ src/components/forms/schemas/formReportUser.ts
@@ -0,0 +1,26 @@
+import { FormSchema } from '../FormSchema';
+import { USER_REPORT_REASONS } from 'src/constants/users';
+
+export const formReportUser: FormSchema<{
+  reason: string;
+  comment: string;
+}> = {
+  id: 'form-add-organization',
+  fields: [
+    {
+      id: 'reason',
+      name: 'reason',
+      component: 'select-simple',
+      title: 'Raison du signalement *',
+      options: USER_REPORT_REASONS,
+      isRequired: true,
+    },
+    {
+      id: 'comment',
+      name: 'comment',
+      component: 'text-input',
+      title: 'Commentaire',
+      isRequired: false,
+    },
+  ],
+};
