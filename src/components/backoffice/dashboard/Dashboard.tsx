e957cc455e79f51649004ba540d38bb1b508992a
Dorian <dorian.pizzato@entourage.social>
2024-09-27 15:48:28 +0200

    Created NetworkDiscoverCard and added to the dashboard

--- src/components/backoffice/dashboard/Dashboard.tsx
+++ src/components/backoffice/dashboard/Dashboard.tsx
@@ -21,10 +21,10 @@ import {
 import { DashboardAlertWhatsappCoach } from './DashboardAlertWhatsappCoach/DashboardAlertWhatsappCoach';
 import { DashboardAvailabilityCard } from './DashboardAvailabilityCard';
 import { DashboardLinkedUserCard } from './DashboardLinkedUserCard';
+import { DashboardNetworkDiscoveryCard } from './DashboardNetworkDiscoverCard';
 import { DashboardOpportunitiesCard } from './DashboardOpportunitiesCard';
 import { DashboardProfileCard } from './DashboardProfileCard';
 import { DashboardReadDocumentsCard } from './DashboardReadDocumentsCard';
-import { DashboardRecommendationsCard } from './DashboardRecommendationsCard';
 import { DashboardReferentCard } from './DashboardReferentCard';
 import { DashboardStepsCard } from './DashboardStepsCard';
 import { DashboardToolboxCard } from './DashboardToolboxCard';
@@ -64,8 +64,10 @@ export const Dashboard = () => {
               <>
                 <DashboardReadDocumentsCard />
                 <DashboardStepsCard />
+                {/* Todo: Replace with DashboardRecommendationsCard when the query is fixed */}
+                {/* <DashboardRecommendationsCard /> */}
+                <DashboardNetworkDiscoveryCard />
                 <DashboardOpportunitiesCard />
-                <DashboardRecommendationsCard />
                 <DashboardToolboxCard />
               </>
             )}
