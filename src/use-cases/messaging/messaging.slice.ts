b179814f8c92c9390d3b356c75433d39ae93334b
Guillaume Cauchois <guillobits@gmail.com>
2024-10-28 13:46:13 +0100

    refacto: postMessageSucceeded

--- src/use-cases/messaging/messaging.slice.ts
+++ src/use-cases/messaging/messaging.slice.ts
@@ -77,14 +77,14 @@ export const slice = createSlice({
           }
         } else if (state.conversations) {
           // Append the new message to the conversation list
-          const idxSelectedConversation = state.conversations?.findIndex(
+          const selectedConvIdx = state.conversations?.findIndex(
             (conversation) => conversation.id === state.selectedConversationId
           );
-          state.conversations[idxSelectedConversation].messages.push(message);
+          state.conversations[selectedConvIdx].messages.push(message);
           state.selectedConversation?.messages.push(message);
 
           // Set the conversation as seen
-          state.conversations[idxSelectedConversation].participants.forEach(
+          state.conversations[selectedConvIdx].participants.forEach(
             (participant) => {
               if (participant.id === message.authorId) {
                 participant.ConversationParticipant.seenAt = message.createdAt;
@@ -93,8 +93,8 @@ export const slice = createSlice({
           );
 
           // Move the conversation to the first position
-          const conversation = state.conversations[idxSelectedConversation];
-          state.conversations.splice(idxSelectedConversation, 1);
+          const conversation = state.conversations[selectedConvIdx];
+          state.conversations.splice(selectedConvIdx, 1);
           state.conversations.unshift(conversation);
         }
       },
