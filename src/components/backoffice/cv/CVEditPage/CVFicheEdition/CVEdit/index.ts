cf7a377b6f5f77d98086d96218458424856b2f9c
Guillaume Cauchois <guillobits@gmail.com>
2024-11-06 17:32:48 +0100

    fix(CVEdit): Review on mobile view

--- src/components/backoffice/cv/CVEditPage/CVFicheEdition/CVEdit/index.ts
+++ src/components/backoffice/cv/CVEditPage/CVFicheEdition/CVEdit/index.ts
@@ -1,5 +1,5 @@
 export { CVEditCareerPath } from './CVEditCareerPath';
 export { CVEditCatchphrase } from './CVEditCatchphrase';
 export { CVEditPicture } from './CVEditPicture';
-export { CVEditReviews } from './CVEditReviews';
+export { CVEditReviews } from './CVEditReviews/CVEditReviews';
 export { CVEditWelcome } from './CVEditWelcome';
