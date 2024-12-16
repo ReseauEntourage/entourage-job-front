0df1c6b3baa650f4fc30e17a6704f189ab037cf5
Guillaume Cauchois <guillobits@gmail.com>
2024-12-16 15:03:27 +0100

    fix: default role in Directory

--- src/pages/backoffice/annuaire.tsx
+++ src/pages/backoffice/annuaire.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { LayoutBackOffice } from 'src/components/backoffice/LayoutBackOffice';
-import { LoadingScreen } from 'src/components/backoffice/LoadingScreen';
 import { Directory } from 'src/components/backoffice/directory/Directory/Directory';
 import { useDirectoryRoleRedirection } from 'src/components/backoffice/directory/useDirectoryRoleRedirection';
-import { useRole } from 'src/hooks/queryParams/useRole';
 
 const Annuaire = () => {
-  const role = useRole();
-
   useDirectoryRoleRedirection();
 
   return (
     <LayoutBackOffice title="Annuaire">
-      {role ? <Directory /> : <LoadingScreen />}
+      <Directory />
     </LayoutBackOffice>
   );
 };
