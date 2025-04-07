8c27b0e64589ab7fc44b7fb825a05af8a5e2ded7
Dorian P. <dorian.pizzato@entourage.social>
2025-04-07 14:53:29 +0200

    [FIX] Bold pour nouveaux messages messagerie (#442)
    * bol new messages inverted
    
    * feedback wording

--- src/components/backoffice/messaging/messaging.utils.ts
+++ src/components/backoffice/messaging/messaging.utils.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import moment from 'moment';
 import { Conversation } from 'src/api/types';
 
@@ -7,9 +6,6 @@ export const conversationHasUnreadMessages = (
 ): boolean => {
   const lastMessage = conversation.messages[0];
   const { seenAt } = conversation;
-  console.log('conversationId', conversation.id);
-  console.log('lastMessage', lastMessage);
-  console.log('seenAt', seenAt);
   const hasUnreadMessages =
     !seenAt || moment(lastMessage.createdAt).isSameOrAfter(seenAt);
   return hasUnreadMessages;
