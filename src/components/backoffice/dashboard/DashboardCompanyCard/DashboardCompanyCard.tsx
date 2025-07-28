a6e9cccd47995a7bf16ba1ae02d6b768a013f263
Guillaume Cauchois <guillobits@gmail.com>
2025-07-28 14:40:52 +0200

    feat(DashboardNextSteps): Handle company admin mode - Steps are now set by context - Add UI for company admin mode - wip: handle admin company nudges

--- src/components/backoffice/dashboard/DashboardCompanyCard/DashboardCompanyCard.tsx
+++ src/components/backoffice/dashboard/DashboardCompanyCard/DashboardCompanyCard.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Company } from '@/src/api/types';
-import { CompanyInviteCollaboratorsModal } from '@/src/components/modals/CompanyInviteCollaboratorsModal/CompanyInviteCollaboratorsModal';
-import { openModal } from '@/src/components/modals/Modal';
 import { Button, Card, Text } from 'src/components/utils';
 import {
   StyledContainer,
@@ -18,9 +16,6 @@ export const DashboardCompanyCard = ({
 }: DashboardCompanyCardProps) => {
   const companySettingsUrl = `/`; // TODO : replace with actual company settings URL
 
-  const openCompanySendInviteModal = () => {
-    openModal(<CompanyInviteCollaboratorsModal companyId={company.id} />);
-  };
   return (
     <Card
       dataTestId="dashboard-profile-card"
@@ -35,15 +30,6 @@ export const DashboardCompanyCard = ({
         <StyledCTAContainer>
           <Button variant="secondary" rounded href={companySettingsUrl}>
             Modifier
-          </Button>
-
-          {/* Before implementing the next step invite collaborators */}
-          <Button
-            variant="secondary"
-            rounded
-            onClick={openCompanySendInviteModal}
-          >
-            Inviter des collaborateurs
           </Button>
         </StyledCTAContainer>
       </StyledContainer>
