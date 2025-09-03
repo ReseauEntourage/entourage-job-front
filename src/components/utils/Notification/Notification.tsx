cdab19b219f6293a10b0a7b1b30841040f7c2271
Guillaume Cauchois <guillobits@gmail.com>
2025-09-03 10:38:23 +0200

    fix(Notification): icon based on notification type

--- src/components/utils/Notification/Notification.tsx
+++ src/components/utils/Notification/Notification.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { LucidIcon } from '../Icons/LucidIcon';
+import React, { useMemo } from 'react';
+import { IconName, LucidIcon } from '../Icons/LucidIcon';
 import { Text } from '../Text';
 import { StyledNotification, StyledTextContainer } from './Notification.styles';
 
@@ -8,9 +8,14 @@ interface NotificationProps {
   message: string;
 }
 export const Notification = ({ type, message }: NotificationProps) => {
+  const iconName: IconName = useMemo(() => {
+    if (type === 'danger') return 'CircleX';
+    return 'CircleCheck';
+  }, [type]);
+
   return (
     <StyledNotification type={type}>
-      <LucidIcon name="CircleCheck" size={20} />
+      <LucidIcon name={iconName} size={20} />
 
       <StyledTextContainer>
         <Text color="white">{message}</Text>
