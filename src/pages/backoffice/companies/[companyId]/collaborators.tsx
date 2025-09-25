06ea74a9c3a40f6f788566c406c286a7f2589fba
Guillaume Cauchois <guillobits@gmail.com>
2025-09-25 17:15:33 +0200

    feat(Company): add functionality to invite collaborators and fetch company with collaborators in redux

--- src/pages/backoffice/companies/[companyId]/collaborators.tsx
+++ src/pages/backoffice/companies/[companyId]/collaborators.tsx
@@ -1,56 +1,30 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Api } from '@/src/api';
-import { CompanyWithUsers } from '@/src/api/types';
 import { CompanyCollaboratorsList } from '@/src/components/backoffice/companies/CompanyCollaboratorsTable/CompanyCollaboratorsTable';
 import { useSelectedCompany } from '@/src/hooks/useSelectedCompany';
-import { notificationsActions } from '@/src/use-cases/notifications';
+import { companyActions } from '@/src/use-cases/company';
 import { LayoutBackOffice } from 'src/components/backoffice/LayoutBackOffice';
 import { LoadingScreen } from 'src/components/backoffice/LoadingScreen';
 
 const CompanyCollaboratorsPage = () => {
   const dispatch = useDispatch();
-  const { selectedCompany } = useSelectedCompany();
+  const {
+    companyId,
+    selectedCompanyWithCollaborators,
+    isFetchCompanyWithCollaboratorsRequested,
+  } = useSelectedCompany();
 
-  const [companyWithCollaborators, setCompanyWithCollaborators] =
-    React.useState<CompanyWithUsers | null>(null);
-  const [fetchCollaboratorsLoading, setFetchCollaboratorsLoading] =
-    React.useState(true);
-
-  /**
-   * Fetches the collaborators of the selected company.
-   * This effect runs when the selected company changes.
-   * It retrieves the company data along with its users and pending invitations.
-   * If the company is not selected, it does not fetch any data.
-   * If an error occurs during the fetch, it dispatches a notification.
-   */
   useEffect(() => {
-    if (selectedCompany && selectedCompany.id) {
-      setFetchCollaboratorsLoading(true);
-      Api.getCompanyByIdWithUsersAndPendingInvitations(selectedCompany.id)
-        .then((response) => {
-          setCompanyWithCollaborators(response.data);
-          setFetchCollaboratorsLoading(false);
-        })
-        .catch((error) => {
-          setFetchCollaboratorsLoading(false);
-          console.error('Error fetching company collaborators:', error);
-          dispatch(
-            notificationsActions.addNotification({
-              type: 'danger',
-              message:
-                "Une erreur s'est produite lors de la récupération des collaborateurs de l'entreprise",
-            })
-          );
-        });
-    }
-  }, [dispatch, selectedCompany]);
+    if (!companyId) return;
+    dispatch(companyActions.fetchSelectedCompanyWithCollaboratorsRequested());
+  }, [dispatch, companyId]);
 
   return (
     <LayoutBackOffice title="Gestion de vos collaborateurs">
-      {companyWithCollaborators && !fetchCollaboratorsLoading ? (
+      {!isFetchCompanyWithCollaboratorsRequested &&
+      selectedCompanyWithCollaborators ? (
         <CompanyCollaboratorsList
-          companyWithCollaborators={companyWithCollaborators}
+          companyWithCollaborators={selectedCompanyWithCollaborators}
         />
       ) : (
         <LoadingScreen />
