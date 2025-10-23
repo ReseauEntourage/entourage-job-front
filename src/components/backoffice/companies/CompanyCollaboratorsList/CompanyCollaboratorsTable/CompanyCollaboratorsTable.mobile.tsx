5df35b02191d5f5e8eba22ec02915a9a58ec1846
Guillaume Cauchois <guillobits@gmail.com>
2025-10-23 16:27:24 +0200

    feat(CompanyCollaboratorsList): mobile view in card view

---
+++ src/components/backoffice/companies/CompanyCollaboratorsList/CompanyCollaboratorsTable/CompanyCollaboratorsTable.mobile.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Text } from 'src/components/utils';
+import {
+  StyledMobileCollaboratorItem,
+  StyledMobileCollaboratorItemField,
+  StyledMobileCollaboratorsContainer,
+} from './CompanyCollaboratorsTable.styles';
+import { CompanyCollaboratorsTableProps } from './CompanyCollaboratorsTable.types';
+
+export const CompanyCollaboratorsTableMobile = ({
+  items,
+}: CompanyCollaboratorsTableProps) => {
+  return (
+    <StyledMobileCollaboratorsContainer>
+      {items.map((item) => (
+        <StyledMobileCollaboratorItem key={item.id}>
+          {/* Display name if not '-' */}
+          {item.name === '-' ? null : (
+            <StyledMobileCollaboratorItemField>
+              <Text size="large" color="mediumGray">
+                Nom
+              </Text>
+              <Text size="large" weight="bold">
+                {item.name}
+              </Text>
+            </StyledMobileCollaboratorItemField>
+          )}
+
+          <StyledMobileCollaboratorItemField>
+            <Text size="large" color="mediumGray">
+              Mail
+            </Text>
+            <Text size="large" underline>
+              {item.email}
+            </Text>
+          </StyledMobileCollaboratorItemField>
+
+          {/* Display connectionCounter if not '-' */}
+          {item.connectionCounter === '-' ? null : (
+            <StyledMobileCollaboratorItemField>
+              <Text size="large" color="mediumGray">
+                Candidat(s) contacté(s)
+              </Text>
+              <Text size="large" weight="bold">
+                {item.connectionCounter}
+              </Text>
+            </StyledMobileCollaboratorItemField>
+          )}
+
+          {/* Display account creation date if not '-' */}
+          {item.accountCreated === '-' ? null : (
+            <StyledMobileCollaboratorItemField>
+              <Text size="large" color="mediumGray">
+                Date de création du compte
+              </Text>
+              <Text size="large">{item.accountCreated}</Text>
+            </StyledMobileCollaboratorItemField>
+          )}
+
+          {/* Display invited date if not '-' */}
+          {item.invitedAt === '-' ? null : (
+            <StyledMobileCollaboratorItemField>
+              <Text size="large" color="mediumGray">
+                Invitation envoyée
+              </Text>
+              <Text size="large">
+                {item.invitedAt} -
+                {item.accountCreated === '-'
+                  ? 'En attente de création'
+                  : 'Compte créé'}
+              </Text>
+            </StyledMobileCollaboratorItemField>
+          )}
+        </StyledMobileCollaboratorItem>
+      ))}
+    </StyledMobileCollaboratorsContainer>
+  );
+};
