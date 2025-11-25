fc85ec907807b57f389bf5cc5347ea4cabbf9267
Guillaume Cauchois <guillobits@gmail.com>
2025-11-25 15:24:17 +0100

    refactor(ImgEvent): update StyledImgContainer props to include width, height, and bgColor

--- src/components/utils/Images/ImgEvent/ImgEvent.styles.ts
+++ src/components/utils/Images/ImgEvent/ImgEvent.styles.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
+import { Color } from '@/src/constants/styles';
 
-export const StyledImgContainer = styled.div<{ size: number }>`
+export const StyledImgContainer = styled.div<{
+  width: number;
+  height: number;
+  bgColor: Color;
+}>`
   display: flex;
   flex-direction: column;
   align-items: center;
