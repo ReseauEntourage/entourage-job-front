5eb2f0f92278c7e612a50d5b7c751b7df8f7d783
Guillaume Cauchois <guillobits@gmail.com>
2026-01-08 17:34:41 +0100

    feat: restructurer la page d'accueil de l'onboarding avec un en-tête personnalisé et un parcours d'intégration

--- src/pages/backoffice/onboarding/index.tsx
+++ src/pages/backoffice/onboarding/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { LayoutBackOffice } from '@/src/components/backoffice/LayoutBackOffice';
-import { Text } from '@/src/components/utils';
+import { HeaderBackoffice } from '@/src/components/headers/HeaderBackoffice';
+import { StyledBackgroundedHeaderBackoffice } from '@/src/components/headers/HeaderBackoffice/HeaderBackoffice.styles';
+import { Section, Text } from '@/src/components/utils';
+import { H4 } from '@/src/components/utils/Headings';
 
 const OnboardingHome = () => {
   return (
     <LayoutBackOffice title="Bien démarrer">
-      <Text>Coming soon: Onboarding</Text>
+      <StyledBackgroundedHeaderBackoffice>
+        <Section className="custom-page">
+          <HeaderBackoffice
+            title="Bienvenue dans la communauté Entourage Pro"
+            description="Merci d’avoir rejoins la communauté. Pour vous accompagner au mieux dans vos premier pas et vous aider à tirer le meilleur parti de notre réseau, nous avons préparé quelques ressources et conseils."
+            noSeparator
+          />
+        </Section>
+      </StyledBackgroundedHeaderBackoffice>
+      <Section className="custom-page">
+        <H4 title="Votre parcours d'intégration : 5 étapes essentielles" />
+        <Text>
+          Un parcours structuré pour vous donner toutes les clés de réussite
+          avant votre première session.
+        </Text>
+      </Section>
     </LayoutBackOffice>
   );
 };
