afbeb69d55ccea63759e4458e3cd999bef70dbe8
Guillaume Cauchois <guillobits@gmail.com>
2026-02-04 11:10:44 +0100

    refactor(Content.styles)

--- src/features/backoffice/onboarding/steps/step-profile-completion/Content.styles.ts
+++ src/features/backoffice/onboarding/steps/step-profile-completion/Content.styles.ts
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
-import { COLORS } from '@/src/constants/styles';
+import { BREAKPOINTS, COLORS } from '@/src/constants/styles';
 
 export const StyledProfileSubHeader = styled.div`
   display: flex;
   gap: 30px;
-  justify-content: space-between;
+  flex-direction: column;
+
+  @media (min-width: ${BREAKPOINTS.desktop}px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `;
 
 export const StyledAccordionHeader = styled.div`
