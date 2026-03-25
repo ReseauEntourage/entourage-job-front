ce356977c15c270d2861650630861169218b300c
Guillaume Cauchois <guillobits@gmail.com>
2026-03-25 17:26:11 +0100

    Ajout de la gestion de la taille de l'iframe en fonction de l'appareil (mobile ou desktop)

--- src/features/backoffice/ressources/aides-locales/CardDataInclusion.tsx
+++ src/features/backoffice/ressources/aides-locales/CardDataInclusion.tsx
@@ -1,12 +1,19 @@
 import { Card } from '@/src/components/ui';
+import { useIsMobile } from '@/src/hooks/utils';
 
 export const CardDataInclusion = () => {
+  const isMobile = useIsMobile();
+
   return (
     <Card title="Rechercher les structures de l'inclusion autour de vous">
       <iframe
-        src={`https://api.data.inclusion.gouv.fr/widget?token=${process.env.NEXT_PUBLIC_DATA_INCLUSION_TOKEN}&score_qualite_minimum=0.9&x=3&y=2&include_remote_services=true`}
+        src={`https://api.data.inclusion.gouv.fr/widget?token=${
+          process.env.NEXT_PUBLIC_DATA_INCLUSION_TOKEN
+        }&score_qualite_minimum=0.9&x=${
+          isMobile ? 1 : 3
+        }&y=2&include_remote_services=true`}
         width="100%"
-        height="900"
+        height={isMobile ? '1100' : '900'}
         style={{ border: 'none' }}
       />
     </Card>
